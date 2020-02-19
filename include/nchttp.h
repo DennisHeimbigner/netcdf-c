712a6201a3d019d4b6e2180e3e63657f493aa590
Dennis Heimbigner <dmh@ucar.edu>
2020-02-19 14:26:38 -0700

    ckp

--- include/nchttp.h
+++ include/nchttp.h
@@ -8,6 +8,10 @@
 #ifndef NCHTTP_H
 #define NCHTTP_H
 
+typedef enum HTTPMETHOD {
+HTTPNONE=0, HTTPGET=1, HTTPPUT=2, HTTPPOST=3, HTTPHEAD=4
+} HTTPMETHOD;
+
 extern int nc_http_open(const char* objecturl, void** curlp, size64_t* filelenp);
 extern int nc_http_read(void* curl, const char* url, size64_t start, size64_t count, NCbytes* buf);
 extern int nc_http_close(void* curl);
