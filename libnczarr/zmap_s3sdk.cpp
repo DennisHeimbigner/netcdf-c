9af4cf8f1465878945ce2f24ccfef129ab91dba8
Dennis Heimbigner <dmh@ucar.edu>
2020-03-26 11:52:02 -0600

    ckp

--- libnczarr/zmap_s3sdk.cpp
+++ libnczarr/zmap_s3sdk.cpp
@@ -34,8 +34,6 @@ typedef struct ZS3MAP {
     NCZMAP map;
     char* bucket; /* bucket name */
     char* region; /* region */
-    char* bucketurl; /* url prefix in path style */
-    Aws::SDKOptions options;
     Aws::Client::ClientConfiguration config;
 } ZS3MAP;
 
@@ -74,7 +72,7 @@ static void
 zs3initialize(void)
 {
     Aws::SDKOptions options;
-    options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
+    options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Error;
     Aws::InitAPI(options);
     zs3initialized = 1;
 }
@@ -146,8 +144,6 @@ zs3create(const char *path, int mode, size64_t flags, void* parameters, NCZMAP**
     z3map->region = region; /* region */
     z3map->bucketurl = bucketurl; /* url prefix in path style */
     bucket = (region = (bucketurl = NULL));
-
-    Aws::InitAPI(z3map->options);
 
     if(mapp) *mapp = (NCZMAP*)z3map;    
 
@@ -235,8 +231,6 @@ zs3open(const char *path, int mode, size64_t flags, void* parameters, NCZMAP** m
     z3map->bucketurl = bucketurl; /* url prefix in path style */
     bucket = (region = (bucketurl = NULL));
 
-    Aws::InitAPI(z3map->options);
-
     z3map->config.scheme = Aws::Http::Scheme::HTTPS;
     z3map->config.region = z3map->region;
     z3map->config.connectTimeoutMs = 30000;
@@ -273,14 +267,23 @@ zs3len(NCZMAP* map, const char* key, size64_t* lenp)
 {
     int stat = NC_NOERR;
     ZS3MAP* z3map = (ZS3MAP*)map;
-
-    if((stat=zs3buildurl(z3map, key)))
+    Aws::S3::S3Client s3_client;
+    Aws::S3::Model::ListObjectsRequest objects_request;
+    objects_request.WithBucket(z3map->bucket);
+    objects_request.SetMarker(key);
+    objects_request.SetMaxKeys(1);
+    auto list_objects_outcome = s3_client.ListObjects(objects_request);
+    if(list_objects_outcome.IsSuccess()) {
+	Aws::Vector<Aws::S3::Model::Object> object_list = list_objects_outcome.GetResult().GetContents();
+	if(object_list.size() != 1) {
+	    {stat = NC_EACCESS; goto done;}
+        if(lenp) *lenp = (size64_t)(object_list.front().GetSize());
+    } else{
+        nclog(NCLOGDEBUG,"%s",list_objects_outcome.GetError().GetExceptionName());
+	stat = NC_ES3;
 	goto done;
-    if((stat = nc_http_size(z3map->curl,ncbytescontents(z3map->url),lenp))
-	{stat = NC_EACCESS; goto done;}
-
+    }
 done:
-    nullfree(objurl);
     return (stat);
 }
 
@@ -295,7 +298,6 @@ zs3define(NCZMAP* map, const char* key, size64_t len)
 	goto done; /* Already exists */
     else if(stat != NC_EACCESS) /* Some other kind of failure */
 	goto done;
-
     if((stat = zcreateobj(???,segments,len,&grpid)))
 	goto done;
 
@@ -314,7 +316,8 @@ zs3read(NCZMAP* map, const char* key, size64_t start, size64_t count, void* cont
     object_request.SetBucket(z3map->bucket);
     object_request.SetKey(key);
     object_request.SetContentLength((long long)count);
-    object_request.SetBody(content);
+    auto writebuf = Aws::MakeShared<oMemStream>();
+    object_request.SetBody(writebuf);
     auto put_object_outcome = s3_client.PutObject(object_request);
     if(!put_object_outcome.IsSuccess()) {
         auto error = put_object_outcome.GetError();
@@ -356,10 +359,8 @@ zs3write(NCZMAP* map, const char* key, size64_t start, size64_t count, const voi
     object_request.SetBucket(z3map->bucket);
     object_request.SetKey(key);
     object_request.SetContentLength((long long)count);
-    auto input_data = Aws::MakeShared<Aws::StringStream>(
-                                  std::ios_base::in | std::ios_base::binary);
-
-    object_request.SetBody(input_data);
+    auto readbuf = Aws::MakeShared<iMemStream>(content,count);
+    object_request.SetBody(readbuf);
     auto put_object_outcome = s3_client.PutObject(object_request);
     if(!put_object_outcome.IsSuccess()) {
         auto error = put_object_outcome.GetError();
@@ -397,8 +398,7 @@ zs3close(NCZMAP* map, int deleteit)
     nullfree(z3map->region);
     nullfree(z3map->bucketurl);
     nullfree(z3map->accountid);
-    ncbytesfree(z3map->url);        
-    ncbytesfree(z3map->buf);
+    ~z3map->config();
     Aws::ShutdownAPI(z3map->options);
 
 done:
@@ -417,312 +417,68 @@ zs3search(NCZMAP* map, const char* prefix, int deep, NClist* matches)
 {
     int stat = NC_NOERR;
     ZS3MAP* z3map = (ZS3MAP*)map;
-    NClist* segments = nclistnew();
-    NCbytes* tmp = ncbytesnew();
-    NCbytes* accpoint = ncbytesnew();
-    ezxml_t* dom = NULL;
-    ezxml_t x;
+    Aws::S3::S3Client s3_client(zs3map->config);
+    Aws::S3::Model::ListObjectsRequest objects_request;
+
+    objects_request.WithBucket(z3map->bucket);
 
     /* Build the search query */
-    ncbytesclear(tmp);
-    ncbytescat(tmp,"list-type=2"
-    ncbytescat(tmp,"&Bucket=");
-        ncbytescat(tmp,z3map->bucket);
     if(!deep)
-        ncbytescat(tmp,"&Delimiter=/");    
-    ncbytescat(tmp,"&Prefix=");
-        ncbytescat(tmp,prefix);
-    /* Build complete URL */
-    ncbytesclear(z3map->url);
-    ncbytescat(z3map->url,"?");
-    ncbytescat(z3map->url,ncbytescontents(tmp));
-    /* Make the request */
-    if((stat = curl_execute(z3map->curl,HTTPGET,z43map->buf,&httpcode)))
+        objects_request.SetDelimiter("/");
+    objects_request.SetPrefix(prefix);
+
+    auto list_objects_outcome = s3_client.ListObjects(objects_request);
+    if(list_objects_outcome.IsSuccess()) {
+        Aws::Vector<Aws::S3::Model::Object> object_list =
+            list_objects_outcome.GetResult().GetContents();
+        for (auto const &s3_object : object_list) {
+	    nclistpush(matches,(const char*)s3_object.GetKey());
+        }
+    } else {
+        nclog(NCLOGERR,"%s: %s",
+            list_objects_outcome.GetError().GetExceptionName(),
+            list_objects_outcome.GetError().GetMessage());
+	stat = NC_ES3;
 	goto done;
-    /* Parse the returned XML */
-    if((dom = ezxml_parse_str(ncbytescontents(z3map->buf),nclistlength(z3map->buf))) == NULL)
-	{stat = NC_ETRANSLATION; goto done;}
-    /* Extract the relevant keys */
-    /* Root should be ListObjectsV2Output */
-    if(strcmp(dom->->name,"ListobjectsV2Ouput")!=0)
-	{stat = NC_ETRANSLATION; goto done;}
-#if 0
-    /* See if truncated result */
-    if((x=ezxml_child(tree, "IsTruncated")) != NULL) {
-	if(strcasecmp(ezxml_txt(x),"true")==0) istruncated = 1;
-    }
-#endif
-    if((x=ezxml_child(dom, "Contents")) == NULL)
-	goto done;
-    for(x=ezxml_child(x, "Key");x != NULL;x = ezxml_next(x)) {
-	char* suffix = ezxml_txt(segment);
-	/* Include this key iff it has no slashes */
-	assert(suffix != NULL && strchr(suffix,'/') == NULL)
-        nclistpush(subnodes,suffix);
     }
 
 done:
     return THROW(stat);
 }
 
-#if 0
-/* Return a list of keys for all child nodes of the parent;
-   It is up to the caller to figure out the type of the node.
-   Assume that parentkey refers to a group; fail otherwise.
-   The list includes subgroups.
-*/
-int
-zs3children(NCZMAP* map, const char* parentkey, NClist* children)
-{
-    int stat = NC_NOERR;
-    ZS3MAP* zs3map = (ZS3MAP*)map;
-    NClist* segments = nclistnew();
-    int grpid;
-    int ngrps;
-    int* subgrps = NULL;
-    int i;
-
-    if((stat=nczm_split(parentkey,segments)))
-	goto done;    
-    if((stat = zlookupgroup(???,segments,0,&grpid)))
-	goto done;
-    /* Start by getting any subgroups */
-    if((stat = nc_inq_grps(grpid,&ngrps,NULL)))
-	goto done;
-    if(ngrps > 0) {
-        if((subgrps = calloc(1,sizeof(int)*ngrps)) == NULL)
-	    {stat = NC_ENOMEM; goto done;}
-        if((stat = nc_inq_grps(grpid,&ngrps,subgrps)))
-	    goto done;
-	/* Get the names of the subgroups */
-	for(i=0;i<ngrps;i++) {
-	    char name[NC_MAX_NAME];
-	    char zname[NC_MAX_NAME];
-	    char* path = NULL;
-	    if((stat = nc_inq_grpname(subgrps[i],name)))
-		goto done;
-	    /* translate name */
-	    zify(name,zname);
-	    /* Create a full path */
-	    if((stat = nczm_suffix(parentkey,zname,&path)))
-		goto done;
-	    /* Add to list of children */
-	    nclistpush(children,path);
-	    path = NULL; /* avoid mem errors */
-	}		
-    }
-
-done:
-    return stat;
-}
-#endif
-
 /**************************************************/
 /* Utilities */
 
-/* Lookup a group by parsed path (segments)*/
-/* Return NC_EACCESS if not found */
+/* Create an object corresponding to a key */
 static int
-z3lookupgroup(ZS3MAP* z3map, NClist* segments, int nskip, int* grpidp)
+z3createobj(ZS3MAP* z3map, const char* key, size64_t size)
 {
     int stat = NC_NOERR;
-    int i, len, grpid;
+    Aws::S3::S3Client s3_client(z3map->clientConfig);
+    Aws::S3::Model::PutObjectRequest object_request;
+    
 
-    len = nclistlength(segments);
-    len += nskip; /* leave off last nskip segments */
-    grpid = Z3MAP->ncid;
-    for(i=0;i<len;i++) {
-	int grpid2;
-	const char* seg = nclistget(segments,i);
-	char s3name[NC_MAX_NAME];
-	s3ify(seg,s3name);	
-	if((stat=nc_inq_grp_ncid(grpid,s3name,&grpid2)))
-	    {stat = NC_EACCESS; goto done;}
-	grpid = grpid2;
-    }
-    /* ok, so grpid should be it */
-    if(grpidp) *grpidp = grpid;
-
-done:
-    return (stat);
-}
-
-/* Lookup an object */
-/* Return NC_EACCESS if not found */
-static int
-z3lookupobj(ZS3MAP* z3map, NClist* segments, int* grpidp)
-{
-    int stat = NC_NOERR;
-    int grpid;
-
-    /* Lookup thru the final object group */
-    if((stat = zlookupgroup(???,segments,0,&grpid)))
+    object_request.SetBucket(z3map->bucket);
+    object_request.SetKey(key);
+    object_request.SetContentLength(0);
+    char* emptydata[1] = 0;
+    struct iMemStream memstream(emptydata,0);
+    const std::shared_ptr<Aws::IOStream> input_data = 
+        Aws::MakeShared<MemStream>(emptydata,0);
+    object_request.SetBody(input_data);
+    auto put_object_outcome = s3_client.PutObject(object_request);
+    if(!put_object_outcome.IsSuccess()) {
+        auto error = put_object_outcome.GetError();
+        nclog(NCLOGERR,"%s: %s",error.GetExceptionName(),error.getMessage());
+        stat = NC_ES3;
 	goto done;
-    if(grpidp) *grpidp = grpid;
-
+    }
 done:
     return (stat);    
 }
-
-/* Create a group; assume all intermediate groups exist
-   (do nothing if it already exists) */
-static int
-z3creategroup(ZS3MAP* z3map, NClist* segments, int nskip, int* grpidp)
-{
-    int stat = NC_NOERR;
-    int i, len, grpid, grpid2;
-    const char* gname = NULL;
-    char s3name[NC_MAX_NAME];
-
-    len = nclistlength(segments);
-    len -= nskip; /* leave off last nskip segments (assume nskip > 0) */
-    gname = nclistget(segments,len-1);
-    grpid = Z3MAP->ncid;
-    /* Do all but last group */
-    for(i=0;i<(len-1);i++) {
-	const char* seg = nclistget(segments,i);
-	/* Does this group exist? */
-	if((stat=nc_inq_grp_ncid(grpid,s3seg,&grpid2)) == NC_ENOGRP) {
-	    {stat = NC_EACCESS; goto done;} /* missing intermediate */
-	}
-	grpid = grpid2;
-    }
-    /* Check status of last group */
-    s3ify(gname,s3name);
-    if((stat = nc_inq_grp_ncid(grpid,s3name,&grpid2))) {
-	if(stat != NC_ENOGRP) goto done;
-        if((stat = nc_def_grp(grpid,s3name,&grpid2)))
-	    goto done;
-	grpid = grpid2;
-    }
-
-    if(grpidp) *grpidp = grpid;
-
-done:
-    return (stat);
-}
-
-/* Create an object group corresponding to a key; create any
-   necessary intermediates.
- */
-static int
-z3createobj(ZS3MAP* z3map, const char* key)
-{
-    int i,nsegs,stat = NC_NOERR;
-    NClist* segments = nclistnew();
-    NCbytes* path = ncbytesnew();
-
-    /* Split key so we can create intermediates */ 
-    if((stat=nczm_split(key,segments)))
-	goto done;
-
-    /* Create the path prefix*/
-    if((stat=zs3buildurl(z3map, NULL)))
-	goto done;
-    nsegs = nclistlength(segments);
-    for(i=0;i <nsegs; i++) {
-	const char* seg = nclistget(segments,i);
-	/* create key for intermediate */
-        switch (stat=zs3extendurl(z3map, seg)) {
-	case NC_NOERR: break; /* exists */
-	case NC_EACCESS: /* create it */
-            if((stat = zcreateobject(z3map,ncbytescontents(z3map->url))))
-		goto done;
-	    break;
-        default: goto done;
-    }
-
-done:
-    return (stat);    
-}
-
-#if 0
-/* Extract the bucket path from a url string */
-static int
-getbucketurl(const char* surl, char** bucketp)
-{
-    int stat = NC_NOERR;
-    NCURI* uri = NULL;
-    char* bucket = NULL;
-    if(!ncuriparse(surl,&uri)) {
-	int blen;
-	/* Check the protocol and extract the file part */	
-	if(strcasecmp(uri->protocol,"s3") != 0)
-	   && strcasecmp(uri->protocol,"http") != 0)
-	   && strcasecmp(uri->protocol,"https") != 0)
-	    {stat = NC_EURL; goto done;}
-        /* Extract protocol + host plus port */	
-	blen = strlen("https://");
-        blen += strlen(uri->host)
-	if(uri->port) blen += strlen(port) + 1;
-	if((bucket = malloc(blen+1)) == NULL)
-	   {stat = NC_ENOMEM; goto done;}
-	bucket[0] = '\0';
-	strlcat(bucket,"https://");
-	strlcat(bucket,uri->host);
-	if(uri->port != NULL) {
-	    strlcat(bucket,":");
-	    strlcat(bucket,uri->port);	    
-	}
-        if(bucketp) {*bucketp = bucket; bucket = NULL;}
-    } else {
-	stat = NC_EACCESS;
-	goto done;
-    }
-
-done:
-    ncurifree(uri);
-    nullfree(bucket);
-    return stat;
-}
-#endif
 
 /**************************************************/
 /* S3 Utilities */
-
-static int
-s3exists(ZS3MAP* z3map)
-{
-    abort();
-}
-
-static int
-s3createbucket(ZS3MAP* z3map)
-{
-    abort();
-}
-
-static int
-s3clear(ZS3MAP* z3map)
-{
-    return NC_NOERR;
-}
-
-static int
-s3bucketpath(const char* bucket, const char* region, char** bucketp)
-{
-    /* Build path-style url.
-       e.g. https://s3.Region.amazonaws.com/bucket-name*/
-    NCbytes* bpath = ncbytesnew();
-    ncbytescat(bpath, "https://s2.");
-    ncbytescat(bpath, region);
-    ncbytescat(bpath, ".amazonaws.com/");
-    ncbytescat(bpath, bucket);
-    if(bucketp) *bucketp = ncbytesextract(bpath);
-    return NC_NOERR;
-}
-
-static int
-s3buildaccesspoint(ZS3MAP* zmap, NCbytes* apoint)
-{
-    ncbytescat(apoint,zmap->accesspointname);
-    ncbytescat(apoint,"-");
-    ncbytescat(apoint,zmap->accountid);
-    ncbytescat(apoint,".s3-accesspoint.");
-    ncbytescat(apoint,zmap->region);
-    ncbytescat(apoint,".amazonaws.com");
-    return NC_NOERR;
-}
 
 static Aws::S3::Model::BucketLocationConstraint
 s3findregion(const char* name)
@@ -730,19 +486,27 @@ s3findregion(const char* name)
     return Aws::S3::Model::BucketLocationConstraint::BucketLocationConstraintMapper::BucketLocationConstraintMapper::GetBucketLocationConstraintForName(name);
 }
 
-struct membuf: std::streambuf {
-    membuf(char const* base, size_t size) {
-        char* p(const_cast<char*>(base));
+struct MemBuf: std::streambuf {
+    MemBuf(char* base, size_t size) {
+        char* p(base);
         this->setg(p, p, p + size);
     }
 };
 
-struct imemstream: virtual membuf, std::istream {
-    imemstream(char const* base, size_t size)
-        : membuf(base, size)
-        , std::istream(static_cast<std::streambuf*>(this)) {
+struct iMemstream: virtual MemBuf, std::istream {
+    iMemstream(char const* base, size_t size)
+        : MemBuf((char*)base, size)
+        , std::ostream(static_cast<std::streambuf*>(this)) {
     }
 };
+
+struct oMemstream: virtual MemBuf, std::ostream {
+    oMemstream(char * base, size_t size)
+        : MemBuf(base, size)
+        , std::ostream(static_cast<std::streambuf*>(this)) {
+    }
+};
+
 /**************************************************/
 /* External API objects */
 
