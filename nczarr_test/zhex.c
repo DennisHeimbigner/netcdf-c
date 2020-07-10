24ef2a06b74345a2c1730e8ef1b1d6f4fc5ec117
Dennis Heimbigner <dmh@ucar.edu>
2020-07-09 19:50:33 -0600

    dehex

---
+++ nczarr_test/zhex.c
@@ -0,0 +1,41 @@
+/*
+ *	Copyright 2018, University Corporation for Atmospheric Research
+ *      See netcdf/COPYRIGHT file for copying and redistribution conditions.
+ */
+
+#include "config.h"
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef HAVE_STDARG_H
+#include <stdarg.h>
+#endif
+
+#include "zincludes.h"
+#include "ncwinpath.h"
+
+#define DEBUG
+
+static char hex[16] = "0123456789abcdef";
+
+int
+main(int argc, char** argv)
+{
+    unsigned char c;
+    unsigned int c0,c1;
+
+//hexdump -v -e '1/1 " %1x"' ./testmap.nzf/data1/0` ;;
+    for(;;) {
+	int ret = fread(&c, 1, 1, stdin);
+	if(ret != 1) break;
+        c1 = c;
+        c0 = c1 & 0xf;
+	c1 = (c1 >> 4);
+        c0 = hex[c0];
+        c1 = hex[c1];
+        printf("%c%c",(char)c1,(char)c0);
+    }
+    return 0;
+}
