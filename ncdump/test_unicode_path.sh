f3c16c34dcd508371cca528637dfcd84621c99cb
Dennis Heimbigner <dmh@ucar.edu>
2020-10-09 16:45:37 -0600

    ckp

--- ncdump/test_unicode_path.sh
+++ ncdump/test_unicode_path.sh
@@ -22,26 +22,26 @@ else
 UNISTRING='海'
 fi
 
-UNIFILE="tst_utf8_${UNISTRING}.nc"
+UNIFILE="tst_utf8_${UNISTRING}"
 
 echo ""
 
-echo "*** Generating netcdf-3 binary file $UNIFILE..."
-${NCGEN} -4 -b -o "${UNIFILE}" "${srcdir}/ref_tst_utf8.cdl"
-echo "*** Accessing binary file ${UNIFILE}..."
-${NCDUMP} -h "${UNIFILE}"
+echo "*** Generating netcdf-3 binary file ${UNIFILE}.nc ..."
+${NCGEN} -4 -b -o "${UNIFILE}.nc" "${srcdir}/ref_tst_utf8.cdl"
+echo "*** Accessing binary file ${UNIFILE}.nc ..."
+${NCDUMP} -h "${UNIFILE}.nc"
 
 if test "x$FEATURE_HDF5" != x ; then
-echo "*** Generating netcdf-3 binary file $UNIFILE..."
-rm -f "${UNIFILE}"
-${NCGEN} -4 -b -o "${UNIFILE}" "${srcdir}/ref_tst_utf8.cdl"
-echo "*** Accessing binary file ${UNIFILE}..."
-${NCDUMP} -h "${UNIFILE}"
+echo "*** Generating netcdf-3 binary file ${UNIFILE}.nc ..."
+rm -f "${UNIFILE}.nc"
+${NCGEN} -4 -b -o "${UNIFILE}.nc" "${srcdir}/ref_tst_utf8.cdl"
+echo "*** Accessing binary file ${UNIFILE}.nc ..."
+${NCDUMP} -h "${UNIFILE}.nc"
 fi
 
 # This test was moved here from tst_nccopy4.sh
 # to unify all the unicode path tests
-echo "*** Test nccopy ${UNIFILe} copy_of_${UNIFILE}.nc ..."
+echo "*** Test nccopy ${UNIFILe} copy_of_${UNIFILE} ..."
 ${NCCOPY} ${UNIFILE}.nc copy_of_${UNIFILE}.nc
 ${NCDUMP} -n copy_of_${UNIFILE} ${UNIFILE}.nc > tmp_${UNIFILE}.cdl
 ${NCDUMP} copy_of_${UNIFILE}.nc > copy_of_${UNIFILE}.cdl
@@ -50,4 +50,4 @@ diff copy_of_${UNIFILE}.cdl tmp_${UNIFILE}.cdl
 rm copy_of_${UNIFILE}.nc copy_of_${UNIFILE}.cdl tmp_${UNIFILE}.cdl
 
 echo "Test Passed. Cleaning up."
-rm ${UNIFILE}
+rm ${UNIFILE}.nc
