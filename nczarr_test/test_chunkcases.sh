9d767ccfd8ecf328ccb37af814820e8db1fb84c5
Dennis Heimbigner <dmh@ucar.edu>
2020-11-28 14:08:51 -0700

    update file permission

--- nczarr_test/test_chunkcases.sh
+++ nczarr_test/test_chunkcases.sh
@@ -16,5 +16,3 @@ ${execdir}/tst_chunkcases -D -4 -d 6,6 -c 6,6 -W -r file://tmp.nzf\#mode=nczarr,
 diff -b ${srcdir}/ref_tmp_whole.txt tmp_whole.txt
 ${NCDUMP} file://tmp.nzf\#mode=nczarr,nzf > tmp_whole.cdl
 diff -b ${srcdir}/ref_tmp_whole.cdl tmp_whole.cdl
-
-exit
