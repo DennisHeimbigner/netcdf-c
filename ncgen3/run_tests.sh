e95d762704445afd48ae917175adcfe85e172d69
Dennis Heimbigner <dmh@ucar.edu>
2021-11-12 12:50:19 -0700

    mingw23

--- ncgen3/run_tests.sh
+++ ncgen3/run_tests.sh
@@ -8,6 +8,17 @@ set -e
 if test "x$srcdir" = x ;then srcdir=`pwd`; fi
 . ../test_common.sh
 
+# We need to find the drive letter, if any
+DL=`${NCPATHCVT} -c -e / | sed -e 's|/cygdrive/\([a-zA-Z]\)/.*|\1|'`
+if test "x$DL" != x ; then
+  # Lower case drive letter
+  DLL=`echo "$DL" | tr '[:upper:]' '[:lower:]'`
+  DL="-d $DLL"
+fi
+echo "MSYS_PREFIX=$MSYS_PREFIX"
+echo "MINGW_PREFIX=$MINGW_PREFIX"
+${NCPATHCVT} -k
+
 echo "*** creating classic file c0.nc from c0.cdl..."
 ${NCGEN3} -b -o c0.nc ${ncgen3c0}
 echo "*** creating 64-bit offset file c0_64.nc from c0.cdl..."
