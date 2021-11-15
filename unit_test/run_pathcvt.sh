0ea17acb288076849ca1e813de4ada3e1ad4ec1e
Dennis Heimbigner <dmh@ucar.edu>
2021-11-14 21:07:13 -0700

    mingw56

--- unit_test/run_pathcvt.sh
+++ unit_test/run_pathcvt.sh
@@ -6,6 +6,11 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 set -e
 set -x
 
+echo "${MSYS_PREFIX}"
+echo "${MSYS2_PREFIX}"
+echo "${MINGW_PREFIX}"
+cygpath -w "/"
+
 exec 2>/dev/null
 cd /usr/local
 pwd -W
