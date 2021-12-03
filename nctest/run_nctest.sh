f705b815c78e1344558ab8c408b9d5a0b6d67fdd
Dennis Heimbigner <dmh@ucar.edu>
2021-12-03 15:05:24 -0700

    testing30

--- nctest/run_nctest.sh
+++ nctest/run_nctest.sh
@@ -6,12 +6,6 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 set -e
 
 export NETCDF_LOG_LEVEL=10
-if ${execdir}/nctest |tee nctest.log ; then
-ret=0
-else
-ret=1
-fi
+${execdir}/nctest 2>1 > nctest.log
 unset NETCDF_LOG_LEVEL
-find . -name '*.nc'
-${execdir}/ncdump/ncdump nctest_netcdf4_classic.nc
 exit $ret
