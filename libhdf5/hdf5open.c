e1982b7a46ac6d0edf77bc3200039fe9187065fa
Dennis Heimbigner <dmh@ucar.edu>
2021-12-03 14:03:38 -0700

    testing27

--- libhdf5/hdf5open.c
+++ libhdf5/hdf5open.c
@@ -12,6 +12,7 @@
 #include "config.h"
 #include "hdf5internal.h"
 #include "hdf5err.h"
+#include "hdf5debug.h"
 #include "ncrc.h"
 #include "ncauth.h"
 #include "ncmodel.h"
--- nctest/run_nctest.sh
+++ nctest/run_nctest.sh
@@ -5,11 +5,13 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 
 set -e
 
+export NETCDF_LOG_LEVEL=10
 if ${execdir}/nctest |tee nctest.log ; then
 ret=0
 else
 ret=1
 fi
+unset NETCDF_LOG_LEVEL
 find . -name '*.nc'
 ${execdir}/ncdump/ncdump nctest_netcdf4_classic.nc
 exit $ret
