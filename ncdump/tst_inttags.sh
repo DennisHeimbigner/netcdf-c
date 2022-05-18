4351fdc851f4c854973563657be93f91a94f257d
Dennis Heimbigner <dmh@ucar.edu>
2022-05-17 20:15:37 -0600

    debug2

--- ncdump/tst_inttags.sh
+++ ncdump/tst_inttags.sh
@@ -10,6 +10,7 @@ echo "*** Test integer constant suffixes"
 echo "*** creating inttags.nc from inttags.cdl..."
 ${NCGEN} -lb -o inttags.nc $srcdir/inttags.cdl
 echo "*** creating tst_inttags.cdl from inttags.nc..."
+../ncdump/ncdump inttags.nc
 ${NCDUMP} inttags.nc > tst_inttags.cdl
 echo "*** comparing tst_inttags.cdl to ref_inttags.nc..."
 diff -b -w tst_inttags.cdl $srcdir/ref_inttags.cdl
