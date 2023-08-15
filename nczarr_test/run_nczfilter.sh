c7b172d790a9247a9478e3ed2cbd12e212ea19c4
Dennis Heimbigner <dmh@ucar.edu>
2023-08-14 20:35:32 -0600

    xfail1

--- nczarr_test/run_nczfilter.sh
+++ nczarr_test/run_nczfilter.sh
@@ -14,8 +14,6 @@ s3isolate "testdir_nczfilter"
 THISDIR=`pwd`
 cd $ISOPATH
 
-echo ">>>>>>"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:D:/a/netcdf-c/netcdf-c/liblib/.libs"
 ${execdir}/test_nczfilter
 
 if test "x$FEATURE_S3TESTS" = xyes ; then s3sdkdelete "/${S3ISOPATH}" ; fi # Cleanup
