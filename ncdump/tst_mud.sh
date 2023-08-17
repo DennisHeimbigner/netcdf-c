180ed78caf89401276543dc093c78b116c99e5cf
Dennis Heimbigner <dmh@ucar.edu>
2023-08-17 13:42:40 -0600

    mud1

--- ncdump/tst_mud.sh
+++ ncdump/tst_mud.sh
@@ -7,29 +7,55 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 # unlimited dimensions.
 
 set -e
+if test "x$TESTNCZARR" = x1 ; then
+. "$srcdir/test_nczarr.sh"
+s3isolate "testdir_mud4"
+THISDIR=`pwd`
+cd $ISOPATH
+fi
 
 echo ""
 echo "*** Testing ncdump output for multiple unlimited dimensions"
-echo "*** creating netcdf file tst_mud4.nc from ref_tst_mud4.cdl ..."
-${NCGEN} -4 -b -o tst_mud4.nc $srcdir/ref_tst_mud4.cdl
-echo "*** creating tst_mud4.cdl from tst_mud4.nc ..."
-${NCDUMP} tst_mud4.nc > tst_mud4.cdl
+
+echo "*** creating netcdf file $file from ref_tst_mud4.cdl ..."
+
+if test "x$TESTNCZARR" = x1 ; then
+fileargs "tmp_mud4_${zfilt}"
+deletemap $zext $file
+file="$fileurl"
+else
+file="tmp_mud4_${zfilt}.nc"
+rm -f $file
+fi
+
+${NCGEN} -4 -b -o $file $srcdir/ref_tst_mud4.cdl
+echo "*** creating tmp_mud4.cdl from $file ..."
+${NCDUMP} $file > tmp_mud4.cdl
 # echo "*** comparing tst_mud4.cdl with ref_tst_mud4.cdl..."
-diff -b tst_mud4.cdl $srcdir/ref_tst_mud4.cdl
-# echo "*** comparing annotation from ncdump -bc tst_mud4.nc with expected output..."
-${NCDUMP} -bc tst_mud4.nc > tst_mud4-bc.cdl
-diff -b tst_mud4-bc.cdl $srcdir/ref_tst_mud4-bc.cdl
+diff -b tmp_mud4.cdl $srcdir/ref_tst_mud4.cdl
+# echo "*** comparing annotation from ncdump -bc $file with expected output..."
+${NCDUMP} -bc $file > tmp_mud4-bc.cdl
+diff -b tmp_mud4-bc.cdl $srcdir/ref_tst_mud4-bc.cdl
+
 # Now test with char arrays instead of ints
-echo "*** creating netcdf file tst_mud4_chars.nc from ref_tst_mud4_chars.cdl ..."
-${NCGEN} -4 -b -o tst_mud4_chars.nc $srcdir/ref_tst_mud4_chars.cdl
-echo "*** creating tst_mud4_chars.cdl from tst_mud4_chars.nc ..."
-${NCDUMP} tst_mud4_chars.nc > tst_mud4_chars.cdl
-# echo "*** comparing tst_mud4_chars.cdl with ref_tst_mud4_chars.cdl..."
-diff -b tst_mud4_chars.cdl $srcdir/ref_tst_mud4_chars.cdl
-exit 0
+if test "x$TESTNCZARR" = x1 ; then
+fileargs "tmp_mud4_chars${zfilt}"
+deletemap $zext $file
+file="$fileurl"
+else
+file="tmp_mud4_chars${zfilt}.nc"
+rm -f $file
+fi
+echo "*** creating netcdf file $file from ref_tst_mud4_chars.cdl ..."
+${NCGEN} -4 -b -o $file $srcdir/ref_tst_mud4_chars.cdl
+echo "*** creating ${file}.cdl from $file ..."
+${NCDUMP} $file > tmp_mud4_chars.cdl
+# echo "*** comparing tmp_mud4_chars.cdl with ref_tst_mud4_chars.cdl..."
+diff -b tmp_mud4_chars.cdl $srcdir/ref_tst_mud4_chars.cdl
+gexit 0
 # unused
 # echo "*** comparing annotation from ncdump -bc tst_mud4_chars.nc with expected output..."
-${NCDUMP} -bc tst_mud4_chars.nc > tst_mud4_chars-bc.cdl
-# diff -b tst_mud4_chars-bc.cdl $srcdir/ref_tst_mud4_chars-bc.cdl
+${NCDUMP} -bc $file > tmp_mud4_chars-bc.cdl
+# diff -b tmp_mud4_chars-bc.cdl $srcdir/ref_tst_mud4_chars-bc.cdl
 echo "*** All ncdump test output for multiple unlimited dimensions passed!"
 exit 0
