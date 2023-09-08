976e1d733191930c096d08c0edb36285b5fff71b
Dennis Heimbigner <dmh@ucar.edu>
2023-09-08 15:18:53 -0600

    ckp

--- nc_test4/tst_put_vars_two_unlim_dim.c
+++ nc_test4/tst_put_vars_two_unlim_dim.c
@@ -26,7 +26,13 @@ See \ref copyright file for more info.
 #define FILE_NAME "tst_put_vars_two_unlim_dim.nc"
 #endif
 
-int main(int argc, char* argv[])
+/* ,,,,, */
+static void ignore()
+{
+}
+
+int
+main(int argc, char* argv[])
 {
     int ret;
     int ncid;
