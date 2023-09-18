350eac6cea354afd57d8a26603b841d1dacc4e9d
Dennis Heimbigner <dmh@ucar.edu>
2023-09-18 15:27:11 -0600

    test1

--- ncdap_test/tst_hyrax.sh
+++ ncdap_test/tst_hyrax.sh
@@ -5,24 +5,24 @@ export srcdir;
 
 . ../test_common.sh
 
-. ${srcdir}/d4test_common.sh
-
 set -e
+
+# Uncomment to get more verbose info
+#VERBOSE=1
+
+isolate testdir_hyrax
+THISDIR=`pwd`
+cd $ISOPATH
 
 echo "test_hyrax.sh:"
 
-# Some test baseline paths are too long for tar.
-TOOLONG="AIRS/AIRH3STM.003/2002.12.01/AIRS.2002.12.01.L3.RetStd_H031.v4.0.21.0.G06101132853.hdf?/TotalCounts_A"
+WD=`pwd`
+cd ${top_srcdir}/ncdap_test/expectedhyrax; BASELINEHY=`pwd` ; cd ${WD}
+TESTSERVER="http://test.opendap.org"
 
 F="\
-RSS/amsre/bmaps_v05/y2006/m01/amsre_20060131v5.dat?/time_a[0:2][0:5] \
-nc4_test_files/nc4_nc_classic_no_comp.nc \
-nc4_test_files/nc4_nc_classic_comp.nc \
-nc4_test_files/nc4_unsigned_types.nc \
-nc4_test_files/nc4_unsigned_types_comp.nc \
-nc4_test_files/nc4_strings.nc \
-nc4_test_files/nc4_strings_comp.nc \
-nc4_test_files/ref_tst_compounds.nc \
+opendap/netcdf/examples/ECMWF_ERA-40_subset.nc?tcw.tcw[1][1][1] \
+opendap/netcdf/examples/cami_0000-09-01_64x128_L26_c030918.nc?hyai.hyai
 "
 
 failure() {
@@ -30,29 +30,16 @@ failure() {
       exit 1
 }
 
-setresultdir results_test_hyrax
-TESTSERVER=`${execdir}/findtestserver dap2 dts`
-if test "x$TESTSERVER" = x ; then
-echo "***XFAIL: Cannot find dts testserver; test skipped"
-exit 0
-fi
-
-gethyraxbaseline() {
-    ARG1="$1"
-    BASELINEPART=`echo $ARG1 | cut -d':' -f1`
-    URLPART=`echo $ARG1 | cut -d':' -f2`    
-    if test "x$BASELINEPART" = "x$URLPART" ; then BASELINEPART=""; fi
-}
-
 makehyraxurl() {
-    URL="${TESTSERVER}/${PREFIX}/${FILE}"
-    URL="$URL&log&show=fetch"
+    URL="${TESTSERVER}/${PREFIX}/${FILE}${QUERY}"
+    URL="$URL#dap2&log"
+    if test "x$VERBOSE" != x ; then URL="$URL&show=fetch"; fi
 }
 
 hyraxsplit() {
     P="$1"
     QUERY=`echo $P | cut -d? -f2`
-    if test "x$QUERY" = "x$P" ; then QUERY="" ; fi
+    if test "x$QUERY" = "x$P" ; then QUERY="" ; else QUERY="?${QUERY}" ; fi
     P=`echo $P | cut -d? -f1`
     FILE=`basename $P`
     PREFIX=`dirname $P`
@@ -63,16 +50,16 @@ for f in $F ; do
   hyraxsplit $f
   makehyraxurl
   echo "testing: $URL"
-  if ! ${NCDUMP} ${DUMPFLAGS} "${URL}" > ./results_test_hyrax/${FILE}.hyrax; then
+  if ! ${NCDUMP} ${DUMPFLAGS} "${URL}" > ./${FILE}.hyrax; then
     failure "${URL}"
   fi
   if test "x${TEST}" = x1 ; then
-    if ! diff -wBb ${BASELINEHY}/${FILE}.hyrax ./results_test_hyrax/${FILE}.hyrax ; then
+    if ! diff -wBb ${BASELINEHY}/${FILE}.hyrax ./${FILE}.hyrax ; then
       failure "diff ${FILE}.hyrax"
     fi
   elif test "x${RESET}" = x1 ; then
     echo "${FILE}:" 
-    cp ./results_test_hyrax/${FILE}.hyrax ${BASELINEHY}/${FILE}.hyrax
+    cp ./${FILE}.hyrax ${BASELINEHY}/${FILE}.hyrax
   fi
 done
 
