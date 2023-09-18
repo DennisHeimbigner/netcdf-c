237a609ac7485b456651bc5d5f8faafc2eefc1f6
Dennis Heimbigner <dmh@ucar.edu>
2023-09-17 19:21:45 -0600

    t1

--- nczarr_test/run_put_vars_two_unlim_dim.sh
+++ nczarr_test/run_put_vars_two_unlim_dim.sh
@@ -3,15 +3,15 @@
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 . ../test_common.sh
 
-. "$srcdir/test_nczarr.sh"
-
 set -e
 set -x
 
-find ${execdir} test_put_vars_two_unlim_dim${ext}
+find ${execdir} 'test_put_vars_two_unlim_dim*'
 
+if test -f ${execdir}/.libs/test_put_vars_two_unlim_dim${ext} ; then
 nm ${execdir}/.libs/test_put_vars_two_unlim_dim${ext}
+fi
 
-${execdir}/test_put_vars_two_unlim_dim${ext}
+#${execdir}/test_put_vars_two_unlim_dim${ext}
 
 exit 0
