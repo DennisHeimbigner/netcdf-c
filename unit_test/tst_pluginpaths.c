fedd0de6922dec7593a547f0e4c56ddb17b7950f
Dennis Heimbigner <dennis.heimbigner@gmail.com>
2024-09-24 19:56:01 -0600

    win1

--- unit_test/tst_pluginpaths.c
+++ unit_test/tst_pluginpaths.c
@@ -208,6 +208,7 @@ parseactionlist(const char* cmds0)
 	descape(dumpoptions.actions[i].name);
 	descape(dumpoptions.actions[i].arg);
 	dumpoptions.actions[i].action = decodeop(dumpoptions.actions[i].name);
+fprintf(stderr,"$$$ [%d] name=|%s| arg=|%d|\n",(int)i,dumpoptions.actions[i].name,dumpoptions.actions[i].arg);
     }
     return;
 }
