05ec16912c01f81a40fd7b2eb32b5c90708ad435
Dennis Heimbigner <dennis.heimbigner@gmail.com>
2024-09-29 21:59:06 -0600

    dfalttest

--- unit_test/run_dfaltpluginpath.sh
+++ unit_test/run_dfaltpluginpath.sh
@@ -5,29 +5,32 @@
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi 
 . ../test_common.sh
 
-set -e
-
 #CMD="valgrind --leak-check=full"
 
 TESTHPP="/tmp;${HOME}"
 
+FAIL=
+
 # Test with no HDF5_PLUGIN_PATH
 unset HDF5_PLUGIN_PATH
 NOHPP1=`ncpluginpath -f global`
-if test "x$NOHPP1" = "" || test "x$NOHPP1" = "" ; then
+if test "x$NOHPP1" = "x/usr/local/hdf5/lib/plugin" || test "x$NOHPP1" = "xC:ProgramData/hdf5/lib/plugin" ; then
 echo "***PASS: default plugin path = |$NOHPP1|"
 else
+FAIL=1
 echo "***FAIL: default plugin path = |$NOHPP1|"
 fi
-exit
 
 # Test with given HDF5_PLUGIN_PATH
 unset HDF5_PLUGIN_PATH
 export HDF5_PLUGIN_PATH="$TESTHPP"
 HPP1=`ncpluginpath -f global`
-if test "x$HPP1" = "$TESTHPP" ; then
+if test "x$HPP1" = "x$TESTHPP" ; then
+echo "***PASS: default plugin path: |$HPP1| HDF5_PLUGIN_PATH=|$HDF5_PLUGIN_PATH|"
 else
-echo "***PASS: default plugin path: |$HPP1| HDF5_PLUGIN_PATH=|"$HDF5_PLUGIN_PATH|"
+FAIL=1
+echo "***FAIL: default plugin path: |$HPP1| HDF5_PLUGIN_PATH=|$HDF5_PLUGIN_PATH|"
 fi
-echo "***FAIL: default plugin path: |$HPP1| HDF5_PLUGIN_PATH=|"$HDF5_PLUGIN_PATH|"
-exit
+
+if test "x$FAIL" != x ; then exit 1; fi
+exit 0
