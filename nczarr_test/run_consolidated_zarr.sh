d29ad1dae70f62d2a1ea0454ebd17f6c5fbe58fa
Dennis Heimbigner <dennis.heimbigner@gmail.com>
2025-01-04 14:09:15 -0700

    debug12

--- nczarr_test/run_consolidated_zarr.sh
+++ nczarr_test/run_consolidated_zarr.sh
@@ -49,18 +49,20 @@ testcase_csl_vs_no(){
     zext=$1
     echo "*** Test: consolidated pure python zarr read; format=$zext"
     deletemap $zext $file
-P=`pwd`
-echo ">>> pwd=${P} srcdir=${srcdir}"
-ls -lrtd ${srcdir}/*.tgz
     tar -zxf $srcdir/ref_consolidated_zarr_2.18.2_python.zarr.tgz
     cp -r ref_consolidated_zarr_2.18.2_python.zarr ref_consolidated_zarr_2.18.2_python.zarr.$zext
     cp -r ref_consolidated_zarr_2.18.2_python.zarr ref_zarr_2.18.2_python.zarr.$zext
     rm -f ref_zarr_2.18.2_python.zarr.$zext/.zmetadata
     fileargs ref_consolidated_zarr_2.18.2_python.zarr "mode=zarr,${zext}"
+echo ">>> (1)"
+P=`pwd`
+ls -lrtd *.file
     ${NCDUMP} -n same_name $fileurl > tmp_consolidated_python_zarr_${zext}.cdl
     fileargs ref_zarr_2.18.2_python.zarr "mode=zarr,$zext"
+echo ">>> (2)"
+ls -lrtd *.zarr
     ${NCDUMP} -n same_name $fileurl > tmp_python_zarr_${zext}.cdl
-    rm -f dif.txt
+    rm -f diff.txt
     diff -b tmp_consolidated_python_zarr_${zext}.cdl tmp_python_zarr_${zext}.cdl > diff.txt
 }
 
